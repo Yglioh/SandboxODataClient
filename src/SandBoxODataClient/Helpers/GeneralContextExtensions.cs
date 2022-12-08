09077b39170637f5ebd97f5da21464a429cf70ac
koen.willaert <koen.willaert@tenforce.com>
2022-12-08 12:03:34 +0100

    Basic setup + helpers

---
+++ src/SandBoxODataClient/Helpers/GeneralContextExtensions.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.OData.Client;
+using Sandbox.OData.Client;
+
+namespace SandboxODataClient.Helpers;
+
+public static class GeneralContextExtensions
+{
+    public static void AddEntity<TEntity>(this Container context, TEntity entity)
+        where TEntity : class
+    {
+        context.AddObject(entity.GetEntitySetName(), entity);
+    }
+
+    public static void AddAndTrackEntity<TEntity>(this Container context, TEntity entity)
+        where TEntity : class
+    {
+        context.AddEntity(entity);
+        var collection = new DataServiceCollection<TEntity>(context);
+        var attachedEntity = (TEntity) context.GetEntityDescriptor(entity).Entity;
+        collection.Load(attachedEntity);
+    }
+
+    /// <inheritdoc cref="DataServiceContext.AttachTo(string, object)"/>
+    public static void AttachEntity<TEntity>(this Container context, TEntity entity)
+        where TEntity : class
+    {
+        context.AttachTo(entity.GetEntitySetName(), entity);
+    }
+
+    /// <summary>
+    /// Attach and track an entity to be able to make use of <see cref="SaveChangesOptions"/>, e.g. <see cref="SaveChangesOptions.PostOnlySetProperties"/>
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <param name="context"></param>
+    /// <param name="entity"></param>
+    public static void AttachAndTrackEntity<TEntity>(this Container context, TEntity entity)
+        where TEntity : class
+    {
+        context.AttachTo(entity.GetEntitySetName(), entity);
+        var collection = new DataServiceCollection<TEntity>(context);
+        var attachedEntity = (TEntity) context.GetEntityDescriptor(entity).Entity;
+        collection.Load(attachedEntity);
+    }
+
+    /// <summary>
+    ///     Gets the entityset name to which the entity belongs.
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <param name="entity"></param>
+    /// <returns></returns>
+    public static string GetEntitySetName<TEntity>(this TEntity entity)
+        where TEntity : class
+    {
+        return entity.GetType().GetCustomAttributes(true)
+            .Where(a => a is EntitySetAttribute t).Cast<EntitySetAttribute>()
+            .FirstOrDefault()!.EntitySet;
+    } 
+
+    /// <summary>
+    ///     Resets tracked entities.
+    /// </summary>
+    /// <param name="context"></param>
+    public static void Clear(this Container context)
+    {
+        var trackedEntities = context.EntityTracker.Entities;
+        foreach (var entity in trackedEntities)
+        {
+            if (!context.Detach(entity.Entity))
+            {
+                throw new Exception($"Unable to dettach entity: {entity.SelfLink}");
+            }
+        }
+    }
+}
--- src/SandBoxODataClient/Program.cs
+++ src/SandBoxODataClient/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.OData.Client;
+using Sandbox.OData.Client;
+using SandboxODataClient.Helpers;
+
+var context = new Container(new Uri("http://localhost:5001/"))
+{
+    MergeOption = MergeOption.NoTracking, // Needed to have nested expand properties filled in
+};
+
+Workspace workspace = new();
+context.AddAndTrackEntity(workspace);
+workspace.Code = "abc";
+workspace.Label = "ABC";
+workspace.IsActive = true;
+
+await context.SaveChangesAsync(SaveChangesOptions.PostOnlySetProperties);
