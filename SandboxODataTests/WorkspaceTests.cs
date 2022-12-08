cfbe018c3bad2be3cdf0ec38fabbc918cfb837c9
Sven Vranckx <sven.vranckx@gmail.com>
2022-12-08 20:23:11 +0100

    Inspect content stream

---
+++ SandboxODataTests/WorkspaceTests.cs
@@ -0,0 +1,39 @@
+using FluentAssertions;
+using Microsoft.OData.Client;
+using Sandbox.OData.Client;
+using SandboxODataClient.Helpers;
+using Xunit.Abstractions;
+
+namespace SandboxODataTests
+{
+    public class WorkspaceTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public WorkspaceTests(ITestOutputHelper output) => _output = output;
+
+        [Fact]
+        public async Task GetWorkspacesTest()
+        {
+            var context = new Context(_output);
+
+            var workspaces = (await context.Workspaces.ExecuteAsync()).ToArray();
+
+            workspaces.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task CreateWorkspaceTest()
+        {
+            var context = new Context(_output, false);
+
+            Workspace workspace = new();
+            context.AddAndTrackEntity(workspace);
+            workspace.Code = "abc";
+            workspace.Label = "ABC";
+            workspace.IsActive = true;
+
+            await context.SaveChangesAsync(SaveChangesOptions.PostOnlySetProperties);
+        }
+    }
+}
